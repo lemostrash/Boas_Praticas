859cf44d9e469bf39df30ec21d79fc98f3b921ea
André de Lemos <53500223+lemostrash@users.noreply.github.com>
2023-06-21 13:33:24 -0300

    Add files via upload

---
+++ Material/Executaveis/Teste/Shell/Basic.bat
@@ -0,0 +1,10 @@
+@echo off
+
+echo Vit233
+
+for /l %%i in (1,1,8) do (
+    start cmd /k "title Aba %%i && echo Executando Aba %%i && setlocal EnableDelayedExpansion && for /l %%n in (1,1,10) do (echo !random!) && endlocal && timeout /t 1 > nul"
+)
+
+timeout /t 10 > nul
+taskkill /f /fi "WINDOWTITLE eq Aba*"
